848775fcad794c4b75db4fe5a57e79e9676142ef
luozhongrui <luozhongrui.cs08@nycu.edu.tw>
2024-11-25 10:13:32 +0800

    siderbar

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,6 +1,10 @@
 import { UserButton } from "@/features/auth/components/user-button";
 import { WorkspaceSwitcher } from "./workspace-switcher";
+import { SidebarButton } from "./sidebar-button";
+import { Bell, Home, MessageSquare, MoreHorizontal } from "lucide-react";
+import { usePathname } from "next/navigation";
 export const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <aside
       style={{
@@ -16,6 +20,14 @@ export const Sidebar = () => {
       }}
     >
       <WorkspaceSwitcher />
+      <SidebarButton
+        icon={Home}
+        label="Home"
+        isActive={pathname.includes("/workspace")}
+      />
+      <SidebarButton icon={MessageSquare} label="DMs" />
+      <SidebarButton icon={Bell} label="Activity" />
+      <SidebarButton icon={MoreHorizontal} label="More" />
       <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
         <UserButton />
       </div>
