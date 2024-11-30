68f4a0651a6626c6208ffc015b762bd58b043e0f
luozhongrui <luozhongrui.cs08@nycu.edu.tw>
2024-11-30 18:39:53 +0800

    fix auth

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,7 +1,7 @@
 import GitHub from "@auth/core/providers/github";
 import Google from "@auth/core/providers/google";
 import { convexAuth } from "@convex-dev/auth/server";
-import { Password} from "@convex-dev/auth/providers/password";
+import { Password} from "@convex-dev/auth/providers/Password";
 import { DataModel } from "./_generated/dataModel";
 
 const CustomPassword = Password<DataModel>({
